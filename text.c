181ad3d91febaf75b06132beb0fca1b483cbaa89
Eric Park <epark03@uoguelph.ca>
2019-10-06 20:09:52 -0400

    Completed base if statements to detect words

--- text.c
+++ text.c
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 #include <math.h>
 #include <ctype.h>
+#include <string.h>
 
 int main() {
 
@@ -32,17 +33,67 @@ int main() {
 struct node_struct *txt2words (FILE *fp) {
 
     printf ("Running txt2words function");
-    char tempLine[256] = "blank";
+    char tempLine[256] = " ";
+    char tempWord[265] = " ";
     char temp = ' ';
+    int wordSize = 0;
+    int i = 0;
 
     while (fgets (tempLine, 256, fp) != NULL) {
-        printf (tempLine);
+        memset(tempWord, 0, sizeof(tempWord));
+        printf ("%s", tempLine);
         /*Read line, detect character then second char if it is word, then store into data, then prep the next struct */
         /*Check first character if its a digit, alphabetical character, hyphen or apostrophe*/
-        if (isdigit(tempLine[0]) != NULL || isalpha(tempLine[0] != NULL || tempLine[0] == '-' || tempLine[0] == ',')) {
+        if (isdigit(tempLine[0]) != 0 || isalpha(tempLine[0]) != 0) {
+            /**/
+            tempWord[0] = tempLine[0];
+            for (i = 1; i < strlen(tempLine); i++) {
+                if ((isdigit(tempLine[i]) == 0 && isalpha(tempLine[i]) == 0) || (tempLine[i] == '-' && tempLine[i+1] == '-') || tempLine[i] == '"') { /*Next char is not a digit nor alpha, or its is a double hyphen, or a double apostrophe*/
+                    /*Stop loop*/
+                    break;
+                }
+                else {
+                    tempWord[i] = tempLine[i];
+                }
+            }
+            printf ("%s\n", tempWord);
 
         }
-
+        else if (strcmp (&tempLine[0], "/n") == 0) {
+            /*just store newline*/
+            tempWord[0] = tempLine[0];
+            printf ("%s\n", tempWord);
+        }
+        else {
+            tempWord[0] = tempLine[0];
+            if (isdigit(tempLine[1]) != 0 || isalpha(tempLine[1] != 0)) { /*Is collection of symbols*/
+                for (i = 1; i < strlen(tempLine); i++) {
+                    if (tempLine[0] != tempLine[i]) { /*Symbols match*/
+                        /*Stop loop*/
+                        break;
+                    }
+                    else {
+                        tempWord[i] = tempLine[i];
+                    }
+                }
+            }
+            else { /*This is type 1 word e.g. regular word*/
+                for (i = 1; i < strlen(tempLine); i++) {
+                    if ((isdigit(tempLine[i]) == 0 && isalpha(tempLine[i]) == 0) || (tempLine[i] == '-' && tempLine[i+1] == '-') || tempLine[i] == '"') { /*Next char is not a digit nor alpha, or its is a double hyphen, or a double apostrophe*/
+                        /*Stop loop*/
+                        break;
+                    }
+                    else {
+                        tempWord[i] = tempLine[i];
+                    }
+                }
+            }
+            printf ("%s\n", tempWord);
+        }
+        /*If first char is char or num, type 1
+         * If first char is newline, type3
+         * If something else, type 2 (symbol repeated)
+         * Else type 1*/
     }
 
     return /*firstnode*/;
@@ -70,8 +121,10 @@ void remove_repeats (struct node_struct *list, int (*compar)(const void *, const
 
 int length (struct node_struct *list) {
 
+    return 0;
 }
 
 void free_list (struct node_struct *list, int free_data) {
+
 
 }
