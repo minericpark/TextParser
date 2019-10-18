9e1786cb64f4a00c8e0d7fe63e14fd1b8bba6b49
Eric Park <epark03@uoguelph.ca>
2019-10-17 21:27:41 -0400

    Completed search and copy functions, fixed print_list helper function to work with linked lists returned by search and copy

--- text.c
+++ text.c
@@ -11,17 +11,35 @@
 int main() {
 
     FILE *fp;
-    struct node_struct *head, *sHead, *test1;
+    struct node_struct *head, *sHead, *test1, *test2, *cHead, *test3;
 
-    if ((fp = fopen("test.txt", "r+")) == NULL) {
+    if ((fp = fopen("1342-0.txt", "r+")) == NULL) {
         printf ("File could not be opened");
     }
+    printf ("\ntxt2words test:\n");
     head = txt2words(fp);
-    print_list(head, 0);
-    sHead = search(head, "EBook", strcmpvoid);
-    print_list(sHead, 1);
-    free_list(sHead, 1);
+    printf ("%d\n", length(head));
+    /*print_list(head, 0);*/
+    printf ("\nsearch test:\n");
+    sHead = search(head, "Chapter", strcmpvoid);
+    printf ("%d\n", length(sHead));
+    /*print_list(sHead, 1);*/
+    test1 = sHead->data;
+    /*printf ("%s\n", (char*)(test1->data));
+    printf ("%p\n", (void*)(test1));*/
+    test2 = ((struct node_struct *) (sHead->next))->data;
+    /*printf ("%s\n", (char*)(test2->data));
+    printf ("%p\n", (void*)(test2));*/
+    printf ("\ncopy test:\n");
+    cHead = copy (test1, test2);
+    printf ("%d\n", length(cHead));
+    /*test3 = cHead->data;
+    printf ("%s\n", (char*)(test3->data));
+    printf ("%p\n", (void*)(test3));*/
+    /*print_list(cHead, 1);*/
     free_list(head, 1);
+    free_list(sHead, 0);
+    free_list(cHead, 0);
     fclose(fp);
 
     return 0;
@@ -51,26 +69,27 @@ int isTypeTwo (char checkSymbol) { /*Verifies if char is symbol excluding some e
     }
 }
 
+/*Function prints the list of the the given linked list at head and relies on type
+ * Type 1 -> Linked list with data->string print, Type 2 -> Linked list with data->nodes print*/
 void print_list (struct node_struct *head, int type) { /*Prints entire linked list*/
-
-    struct node_struct *temp;
 
     if (type == 0) {
         while (head != NULL) {
-            printf ("%s\n", head->data);
+            printf ("%s\n", (char *) head->data);
             head = head->next;
         }
     }
     else {
-        temp = head->data;
-        while (temp != NULL) {
-            printf ("%s\n", (char *)(temp->data));
-            temp = head->next;
+        while (head != NULL) {
+            printf ("%s\n", (char *)((struct node_struct *)(head->data))->data);
+            /*printf ("%p\n", (void*)(head->data));*/
+            head = head->next;
         }
     }
 
 }
 
+/*Function compares two void pointers and returns 1 if they are equal, or 0 otherwise*/
 int strcmpvoid (const void *a, const void *b) {
     char *ptr_a, *ptr_b;
 
@@ -95,62 +114,12 @@ int strcmpvoid (const void *a, const void *b) {
  * node of linked list*/
 struct node_struct *txt2words (FILE *fp) {
 
-    printf ("Running txt2words function\n");
     struct node_struct *head, **ptr;
     char tempLine[256] = " ";
     char tempWord[265] = " ";
-    /*char *storeWord;
-    const char *split = " \n";
-    char tempSym = ' ';*/
     int wordSize = 0;
     int i = 0;
-    /*int j = 0;
-    int type = 0;*/
 
-    /*If first letter of word char/num -> type 1 (isalpha, isdigit)
-     * If first letter of word sym -> type 2 (ispunt)
-     * If first letter of word newline -> type 3 (match newline)
-     * Else type 1*/
-
-    /*Split words in line with strtok (from spaces)*/
-    /*Check word given*/
-/*
-    ptr = &head;
-    while (fgets (tempLine, sizeof(tempLine), fp) != NULL) {
-
-        if (strcmp(tempLine, "\n") == 0) {
-            printf ("newline found\n");
-            storeWord = strtok(tempLine, " ");
-        }
-        else {
-            storeWord = strtok(tempLine, split);
-        }
-        printf ("%s %c\n", storeWord, storeWord[0]);
-        if (isdigit(storeWord[0]) != 0 || isalpha(storeWord[0]) != 0) {
-            printf ("type one\n");
-        }
-        else if (ispunct(storeWord[i]) != 0) {
-            printf("type two\n");
-        }
-        else {
-            printf ("either type ")
-        }
-        while (storeWord != NULL) {
-            for (i = 0; i < strlen(storeWord); i++) {
-                if (isTypeOne(storeWord[i]) != 0) {
-                    printf ("still type1\n");
-                }
-                else if (ispunct(storeWord[i]) != 0) {
-                    printf ("sym detected\n");
-                }
-                else {
-                    printf ("strange line detected\n");
-                }
-            }
-            storeWord = strtok(NULL, split);
-        }
-    }
-*/
     fseek(fp, 0, SEEK_SET);
     ptr = &head; /*Set pointer to address of head*/
     while (fgets (tempLine, sizeof(tempLine), fp) != NULL) { /*Run through entire text of given file*/
@@ -262,6 +231,8 @@ struct node_struct *txt2words (FILE *fp) {
     return head;
 }
 
+/*Function searches through linked list list to find target with a compar function provided. Returns a
+ * pointer to the head of the new linked list composed of the nodes to the found targets*/
 struct node_struct *search (struct node_struct *list, char *target, int (*compar)(const void *, const void *)) {
     struct node_struct *temp = list;
     struct node_struct **ptr, *head;
@@ -271,8 +242,8 @@ struct node_struct *search (struct node_struct *list, char *target, int (*compar
         /*Compare value to of data to target*/
         if (compar (target, temp->data) == 1) {
             /*Then found*/
-            printf ("found\n");
-            /*printf ("%p\n", (void *)(temp));*/
+            /*printf ("found\n");
+            printf ("%p\n", (void *)(temp));*/
             *ptr = malloc (sizeof(struct node_struct));
             (*ptr)->data = malloc (sizeof(struct node_struct));
             (*ptr)->data = &(*temp);
@@ -281,8 +252,35 @@ struct node_struct *search (struct node_struct *list, char *target, int (*compar
         temp = temp->next;
     }
     *ptr = NULL;
-
     /*printf (length(head));*/
+
+    return head;
+}
+
+struct node_struct *copy (struct node_struct *start, struct node_struct *end) {
+
+    struct node_struct *temp = start; /*Assign start to temp*/
+    struct node_struct **ptr, *head;
+
+    ptr = &head;
+    /*printf ("%p\n", (void *)(head));*/
+    while (temp != NULL) { /*Run through entire list given*/
+        if (temp == end) { /*Determines if temp has reached end, then exits loop*/
+            /*Then found*/
+            /*printf ("done\n");*/
+            break;
+        }
+        /*printf ("%p\n", (void *)(temp));
+        printf ("%s\n", (char *)(temp->data));*/
+        *ptr = malloc (sizeof(struct node_struct));
+        (*ptr)->data = malloc (sizeof(struct node_struct));
+        (*ptr)->data = &(*temp);
+        ptr = &((*ptr)->next);
+        temp = temp->next;
+    }
+    *ptr = NULL;
+
+    /*printf ("%p\n", (void *)(head));*/
 
     return head;
 }
@@ -325,10 +323,6 @@ void free_list (struct node_struct *list, int free_data) {
 }
 
 /*
-
-struct node_struct *copy (struct node_struct *start, struct node_struct *end) {
-
-}
 
 void ftext (FILE *fp, struct node_struct *list) {
 
